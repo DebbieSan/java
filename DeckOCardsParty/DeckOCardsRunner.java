45ea30ba34d641e7e307a741daa330dff66fd476
Debbie San <debbie.san16@gmail.com>
2026-06-10 16:43:32 -0600

    Fifth Iteration - program is ready to run but it is hardcoded at the moment

--- DeckOCardsParty/DeckOCardsRunner.java
+++ DeckOCardsParty/DeckOCardsRunner.java
@@ -5,10 +5,15 @@ public class DeckOCardsRunner {
 
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
+=======
+        // Start the party and build a fresh deck
+>>>>>>> Stashed changes
         DeckOCardsParty deck = new DeckOCardsParty();
 
         deck.createDeck();
 
+<<<<<<< Updated upstream
         // deck.printDeck(); // Should print the 52 cards in the deck in order
 
         System.out.println("Dealing the first card...");
@@ -30,6 +35,26 @@ public class DeckOCardsRunner {
         System.out.println("\n--- Deck After Shuffle ---");
         deck.printDeck(); // Deck is now Randomized
 
+=======
+        // Shuffle it up!
+        System.out.println("Shuffling the deck...");
+        deck.shuffleDeck();
+
+        // Deal a 5-card Poker hand
+        System.out.println("\n--- Dealing a 5-card hand to Player 1 ---");
+        DeckOCardsParty.Node[] playerHand = deck.dealHand(5);
+
+        // Print out the player's hand
+        for (int i = 0; i < playerHand.length; i++) {
+            if (playerHand[i] != null) {
+                System.out.println("Card " + (i + 1) + ": " + playerHand[i]);
+            }
+        }
+
+        // See how many cards are left in the deck
+        System.out.println("\n--- Remaining cards in the deck ---");
+        deck.printDeck(); // Should show 47 cards left!
+>>>>>>> Stashed changes
     }
 
 }
