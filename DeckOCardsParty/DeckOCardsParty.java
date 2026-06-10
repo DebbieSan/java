fe328ba0081cdad7a39fe76180a41ee2b757b07e
Debbie San <debbie.san16@gmail.com>
2026-06-10 15:38:42 -0600

    Added more functionality to the software and a new class to host Main, this will be the testing class

--- DeckOCardsParty/DeckOCardsParty.java
+++ DeckOCardsParty/DeckOCardsParty.java
@@ -1,6 +1,6 @@
 public class DeckOCardsParty {
 
-    Node head; // The starting point of the list, initially set to null
+    Node head; // The starting point of the deck (the top card)
 
     class Node {
 
@@ -12,7 +12,9 @@ public class DeckOCardsParty {
 
         Node previous;
 
-        // Node constructor, initializes the node with data and sets links to null.
+        // Node constructor, initializes the node with data, which in the case of a deck
+        // of cards are the suit and rank,
+        // and sets links to null.
 
         Node(String suit, String rank) {
             this.suit = suit;
@@ -24,10 +26,55 @@ public class DeckOCardsParty {
             this.previous = null; // Initially, there is not previous node linked
         }
 
+        // Helper method to easily print the card shuffler and dealer
+        public String toString() {
+            return rank + " of " + suit;
+
+        }
+
+    }
+
+    public void createDeck() {
+        String[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        String[] ranks = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+        Node currentTail = null;
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                Node newCard = new Node(suit, rank);
+                // Add the card to the deck
+
+                if (head == null) {
+                    head = newCard;
+                    currentTail = newCard;
+                } else {
+                    currentTail.next = newCard;
+                    newCard.previous = currentTail;
+                    currentTail = newCard;
+                }
+            }
+
+        }
     }
 
     public DeckOCardsParty() {
-        this.head = null; // At the start , the list is empty so the head is null
+        this.head = null; // The deck starts completely empty
+    }
+
+    /**
+     * A helper method to print an entire deck from top to bottom
+     * to test if the createDeck() method works correctly
+     */
+    public void printDeck() {
+        Node current = head;
+        int count = 1;
+        while (current != null) {
+            System.out.println(count + ": " + current); // This will call the toString() method
+            current = current.next;
+            count++;
+        }
+
     }
 
 }
---
+++ DeckOCardsParty/DeckOCardsRunner.java
@@ -0,0 +1,15 @@
+public class DeckOCardsRunner {
+
+    public static void main(String[] args) {
+
+        DeckOCardsParty deck = new DeckOCardsParty();
+
+        deck.createDeck();
+
+        deck.printDeck(); // Should print the 52 cards in the deck in order
+
+    }
+
+
+    
+}
